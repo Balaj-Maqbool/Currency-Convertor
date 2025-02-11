f689fcf1d0796e659ed678575cc9fae858eb5c05
Balaj Maqbool <balajmaqbool54@gmail.com>
2025-02-11 20:22:38 +0500

    adding ressponsivness

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 // const base_url =  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
-console.time("time taken : ");
 const url = "https://api.exchangerate-api.com/v4/latest/USD";
-const dropdownSelects = document.querySelectorAll(".dropdown select");
-const btn = document.querySelector(".btn");
-const msg = document.querySelector(".msg");
-const from = document.querySelector(".selectfrom");
-const to = document.querySelector(".selectto");
+const dropdownSelects = document.querySelectorAll("select"); // Selects all dropdowns
+const btn = document.querySelector(".convert-btn");
+const msg = document.querySelector(".exchange-message");
+const from = document.querySelector("#from-currency");
+const to = document.querySelector("#to-currency");
+import {countryList} from "./codes.js"
+// List of currency codes mapped to country codes
 
-for (select of dropdownSelects) {
-  for (currCode in countryList) {
+// Populating dropdowns with currency codes
+for (let select of dropdownSelects) {
+  for (let currCode in countryList) {
     let newOption = document.createElement("option");
     newOption.innerText = currCode;
     newOption.value = currCode;
-    if (select.name === "from" && currCode === "USD") {
+
+    // Set default values
+    if (select.id === "from-currency" && currCode === "USD") {
       newOption.selected = "selected";
-    } else if (select.name === "to" && currCode === "PKR") {
+    } else if (select.id === "to-currency" && currCode === "PKR") {
       newOption.selected = "selected";
     }
+
     select.append(newOption);
   }
+
+  // Event listener to update flag when currency changes
   select.addEventListener("change", (evt) => {
     evt.preventDefault();
     updateFlag(evt.target);
   });
 }
+
+// Function to update flag when currency changes
 const updateFlag = (element) => {
   let currCode = element.value;
   let countryCode = countryList[currCode];
@@ -31,22 +40,33 @@ const updateFlag = (element) => {
   let img = element.parentElement.querySelector("img");
   img.src = newSrc;
 };
+
+// Event listener for Convert button
 btn.addEventListener("click", async (evt) => {
   evt.preventDefault();
-  let amount = document.querySelector("#amt");
+
+  let amount = document.querySelector("#amount");
   let amtVal = amount.value;
+
+  // Default to 1 if input is empty or invalid
   if (amtVal === "" || amtVal <= 0) {
     amtVal = "1";
     amount.value = "1";
   }
-  let response = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
-  // console.log(response);
-  let data = await response.json();
-  // const url = `${base_url}/${from.value.toLowerCase()}/${to.value.toLowerCase()}.json`;
-  // let response = await fetch(url);
-  // let data = await response.json();
-  let rate = data.rates[to.value] / data.rates[from.value];
-  let finalAmount = amtVal * rate;
-  msg.innerText = `${amtVal} ${from.value} = ${finalAmount} ${to.value} approx`;
-  console.timeEnd("time taken : ");
+
+  try {
+    let response = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
+    let data = await response.json();
+
+    // const url = `${base_url}/${from.value.toLowerCase()}/${to.value.toLowerCase()}.json`;
+    // let response = await fetch(url);
+    // let data = await response.json();
+
+    let rate = data.rates[to.value] / data.rates[from.value];
+    let finalAmount = (amtVal * rate).toFixed(2);
+
+    msg.innerText = `${amtVal} ${from.value} = ${finalAmount} ${to.value} approx`;
+  } catch (error) {
+    msg.innerText = "Error fetching exchange rates. Try again!";
+  }
 });
